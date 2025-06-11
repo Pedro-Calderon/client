bb7ad02491e7e15048801667d53db6fabff0356c
Pedro-Calderon <s18120164@alumnos.itsur.edu.mx>
2025-06-11 10:44:25 -0600

    add configuration devtools

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+reactStrictMode: true,
+  env: {
+    SHOW_DEV_TOOLS: process.env.NODE_ENV === "development" ? "true" : "false",
+  },
 };
 
 export default nextConfig;
